99b0150d005114ee6f997dfebf68102dec63cdd5
Ashish <shinyflame007@gmail.com>
2022-01-01 13:00:20 -0500

    Testing nRF24L01 module on main code

--- README.md
+++ README.md
@@ -1,9 +1,11 @@
 # Omnidirectional-Robot-Car
 A robot that has the ability to move in more directions than usually possible using unique mecanum wheels.
-
 Controlled using Joystick and NRF24 module.
-
 Made in Platform IO.
+
+## Instructions
+- Use the Joystick_Receiver.ino in the src/Extras folder
+- More coming soon
 
 ## Images
 Rendered pictures of the 3D model I created using parts from [GrabCAD](https://grabcad.com/library) combined with my own models designed for 3D printing. Made in real-world scale and using `Solidworks`.
--- src/Extras/AllDirectionMovement.ino
+++ src/Extras/AllDirectionMovement.ino
@@ -1,28 +1,6 @@
 // Use with 4 yellow motors.
 
 #include <Arduino.h>
-#include <SPI.h>
-#include <nRF24L01.h>
-#include <RF24.h>
-
-RF24 radio(32, 33);   // nRF24L01 (CE, CSN)
-const byte address[6] = "00001";
-unsigned long lastReceiveTime = 0;
-unsigned long currentTime = 0;
-// Max size of this struct is 32 bytes - NRF24L01 buffer limit
-struct Data_Package {
-  byte j1PotX;
-  byte j1PotY;
-  byte j1Button;
-  byte j2PotX;
-  byte j2PotY;
-  byte j2Button;
-  byte button1;
-  byte button2;
-  byte button3;
-  byte button4;
-};
-Data_Package data; //Create a variable with the above structure
 
 // Constants for Interrupt Pins
 const byte MOTOR_A = 3;  // Motor 1 Interrupt Pin - INT 1 - Front Right Motor
@@ -329,107 +307,34 @@ void attachInterrupts()
   attachInterrupt(digitalPinToInterrupt(MOTOR_D), ISR_countD, RISING); // Increase counter D when speed sensor pin goes High
 }
 
-void resetData() {
-  // Reset the values when there is no radio connection - Set initial default values
-  data.j1PotX = 127;
-  data.j1PotY = 127;
-  data.j2PotX = 127;
-  data.j2PotY = 127;
-  data.j1Button = 1;
-  data.j2Button = 1;
-  data.button1 = 1;
-  data.button2 = 1;
-  data.button3 = 1;
-  data.button4 = 1;
-}
-
 void setup()
 {
-
-  Serial.begin(9600);
-  radio.begin();
-  radio.openReadingPipe(0, address);
-  radio.setAutoAck(false);
-  radio.setDataRate(RF24_250KBPS);
-  radio.setPALevel(RF24_PA_MIN); // It was originally LOW
-  radio.startListening(); //  Set the module as receiver
-  resetData();
-
   attachInterrupts(); // Attach the Interrupts to their ISR's
 
   // Test Motor Movement:
-  // MoveForward(CMtoSteps(20), 255); // Ex: Forward at 255 speed for 20cm
-  // delay(1000);                     // Wait one second
-  // MoveReverse(CMtoSteps(20), 255);
-  // delay(1000);
-  // MoveRight(CMtoSteps(20), 255);
-  // delay(1000);
-  // MoveLeft(CMtoSteps(20), 255);
-  // delay(1000);
-  // MoveLeftForward(CMtoSteps(20), 255);
-  // delay(1000);
-  // MoveRightForward(CMtoSteps(20), 255);
-  // delay(1000);
-  // MoveLeftReverse(CMtoSteps(20), 255);
-  // delay(1000);
-  // MoveRightReverse(CMtoSteps(20), 255);
-  // delay(1000);
-  // SpinLeft(CMtoSteps(20), 255);
-  // delay(1000);
-  // SpinRight(CMtoSteps(20), 255);
-  // delay(1000);
+  MoveForward(CMtoSteps(20), 255); // Ex: Forward at 255 speed for 20cm
+  delay(1000);                     // Wait one second
+  MoveReverse(CMtoSteps(20), 255);
+  delay(1000);
+  MoveRight(CMtoSteps(20), 255);
+  delay(1000);
+  MoveLeft(CMtoSteps(20), 255);
+  delay(1000);
+  MoveLeftForward(CMtoSteps(20), 255);
+  delay(1000);
+  MoveRightForward(CMtoSteps(20), 255);
+  delay(1000);
+  MoveLeftReverse(CMtoSteps(20), 255);
+  delay(1000);
+  MoveRightReverse(CMtoSteps(20), 255);
+  delay(1000);
+  SpinLeft(CMtoSteps(20), 255);
+  delay(1000);
+  SpinRight(CMtoSteps(20), 255);
+  delay(1000);
 }
 
 void loop()
 {
-  // Check whether there is data to be received
-  if (radio.available()) {
-    radio.read(&data, sizeof(Data_Package)); // Read the whole data and store it into the 'data' structure
-    lastReceiveTime = millis(); // At this moment we have received the data
-  }
-  // Check whether we keep receving data, or we have a connection between the two modules
-  currentTime = millis();
-  if ( currentTime - lastReceiveTime > 1000 ) { // If current time is more then 1 second since we have recived the last data, that means we have lost connection
-    resetData(); // If connection is lost, reset the data. It prevents unwanted behavior, for example if a drone has a throttle up and we lose connection, it can keep flying unless we reset the values
-  }
-  // Print the data in the Serial Monitor
-  
-  // Use this portion below to see in the SERIAL PLOTTER
-  
-  Serial.print(data.j1PotX);
-  Serial.print(" ");
-  Serial.print(data.j1PotY);
-  Serial.print(" ");
-  Serial.print(data.j2PotX);
-  Serial.print(" ");
-  Serial.print(data.j2PotY);
-  Serial.print(" ");
-  Serial.print(data.button1);
-  Serial.print(" ");
-  Serial.print(data.button2);
-  Serial.print(" ");
-  Serial.print(data.button3);
-  Serial.print(" ");
-  Serial.print(data.button4);
-  Serial.println("");
-
-  // Use this portion below to see in SERIAL MONITOR
-
-  //  Serial.print("j1PotX: ");
-  //  Serial.print(data.j1PotX);
-  //  Serial.print("; j1PotY: ");
-  //  Serial.print(data.j1PotY);
-  //  Serial.print("; j2PotX: ");
-  //  Serial.println(data.j2PotX);
-  //  Serial.print("; j2PotY: ");
-  //  Serial.println(data.j2PotY);
-  //  Serial.print("; button1: ");
-  //  Serial.print(data.button1);
-  //  Serial.print("; button2: ");
-  //  Serial.print(data.button2);
-  //  Serial.print("; button3: ");
-  //  Serial.print(data.button3);
-  //  Serial.print("; button4: ");
-  //  Serial.print(data.button4);
-  //  Serial.println("");
+  // Put whatever you want here!
 }
